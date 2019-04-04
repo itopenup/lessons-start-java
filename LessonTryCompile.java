f9e5060b96e1f2bff2b75988d2636ea79f1478a0
Konstantin <ka.litovchenko@ya.ru>
2019-04-04 14:03:52 +0300

    First sample program created

---
+++ LessonTryCompile.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+class LessonTryCompile {
+	public static void main (String[] arguments) {
+		System.out.println("I'm alive!");
+	}
+}
